97a587357573d1010361aa98a0f12825c66c1fa9
Tewter73 <151186194+Tewter73@users.noreply.github.com>
2026-01-29 11:27:05 +0700

    Add files via upload

---
+++ Year1-Semester2/Progtech-Java/LAB EXAM PRACTICE/LAB 9 Practice/project Q1/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int power(int b, int p) {
+        if (p == 0) {
+            return 1;
+        } else if (p % 2 == 0) {
+            int evenPower = power(b, p /2);
+            return evenPower * evenPower;
+        } else {
+            return b * power(b, p-1);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter x and y: ");
+        int x = input.nextInt();
+        int y = input.nextInt();
+        System.out.println(power(x,y));
+    }
+}
